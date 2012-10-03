e66beef5dcc6c5565bc3c01c6bdb8bca9d4279c9
Eric Berry <eberry@tricky-mac.1on1.com>
2012-10-03 11:41:58 -0600

    initial commit

---
+++ grails-app/controllers/exampleairbrake/AirbrakeTestController.groovy
@@ -0,0 +1,11 @@
+package exampleairbrake
+
+class AirbrakeTestController {
+
+    def throwException() {
+        session.myval = "Something in the Session!"
+        session.password = "super_secret_password"
+        throw new Exception("TestException ${System.currentTimeMillis()}")
+    }
+
+}
